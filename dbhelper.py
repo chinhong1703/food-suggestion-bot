914a13fb7fe3fd12a7f8802d468ba9566870101d
chinhong1703 <44459982+chinhong1703@users.noreply.github.com>
2023-09-30 18:00:29 +0800

    close db connections

--- dbhelper.py
+++ dbhelper.py
@@ -4,66 +4,123 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 
 
 class DBHelperFood:
-  def __init__(self):
-    self.dbname = os.environ['DATABASE_URL']
-    self.engine = create_engine(self.dbname)
-    self.cursor = scoped_session(sessionmaker(bind=self.engine))
-    
-  def setup(self):
-    tblstmt = "CREATE TABLE IF NOT EXISTS items (description text, owner integer)"
-    itemidx = "CREATE INDEX IF NOT EXISTS itemIndex ON items (description ASC)"
-    ownidx = "CREATE INDEX IF NOT EXISTS ownIndex ON items (owner ASC)"
-    self.cursor.execute(tblstmt)
-    self.cursor.execute(itemidx)
-    self.cursor.execute(ownidx)
-    self.cursor.commit()
-    
-  def add_item(self, item_text, owner):
-    stmt = "INSERT INTO items (description, owner) VALUES (:description, :owner)"
-    args = {"description":item_text, "owner":owner}
-    self.cursor.execute(stmt, args)
-    self.cursor.commit()
-  def delete_item(self, item_text, owner):
-    stmt = "DELETE FROM items WHERE description = :description AND owner = :owner"
-    args = {"description":item_text, "owner":owner}
-    self.cursor.execute(stmt, args)
-    self.cursor.commit()
+    def __init__(self):
+        self.dbname = os.environ['DATABASE_URL']
+        self.engine = create_engine(self.dbname)
+        self.cursor = self.session()
 
-  def get_items(self, owner):
-    stmt = "SELECT description FROM items WHERE owner = :owner"
-    args = {"owner":owner}
-    results = self.cursor.execute(stmt, args)
-    return [x[0] for x in results]
+    def session(self):
+        return scoped_session(sessionmaker(bind=self.engine))
+
+    def setup(self):
+        tblstmt = "CREATE TABLE IF NOT EXISTS items (description text, owner integer)"
+        itemidx = "CREATE INDEX IF NOT EXISTS itemIndex ON items (description ASC)"
+        ownidx = "CREATE INDEX IF NOT EXISTS ownIndex ON items (owner ASC)"
+        try:
+            self.cursor.execute(tblstmt)
+            self.cursor.execute(itemidx)
+            self.cursor.execute(ownidx)
+            self.cursor.commit()
+        except:
+            self.cursor.rollback()
+            raise
+        finally:
+            self.cursor.close()
+
+    def add_item(self, item_text, owner):
+        stmt = "INSERT INTO items (description, owner) VALUES (:description, :owner)"
+        args = {"description": item_text, "owner": owner}
+        try:
+            self.cursor.execute(stmt, args)
+            self.cursor.commit()
+        except:
+            self.cursor.rollback()
+            raise
+        finally:
+            self.cursor.close()
+
+    def delete_item(self, item_text, owner):
+        stmt = "DELETE FROM items WHERE description = :description AND owner = :owner"
+        args = {"description": item_text, "owner": owner}
+        try:
+            self.cursor.execute(stmt, args)
+            self.cursor.commit()
+        except:
+            self.cursor.rollback()
+            raise
+        finally:
+            self.cursor.close()
+
+    def get_items(self, owner):
+        stmt = "SELECT description FROM items WHERE owner = :owner"
+        args = {"owner": owner}
+        try:
+            results = self.cursor.execute(stmt, args)
+            return [x for x in results]
+        except:
+            self.cursor.rollback()
+            raise
+        finally:
+            self.cursor.close()
+
+
 
 class DBHelperLog:
-  def __init__(self):
-    self.dbname = os.environ['DATABASE_URL']
-    self.engine = create_engine(self.dbname)
-    self.cursor = scoped_session(sessionmaker(bind=self.engine))
-    
-  def setup(self):
-    tblstmt = "CREATE TABLE IF NOT EXISTS log (description text, owner integer, date timestamp)"
-    itemidx = "CREATE INDEX IF NOT EXISTS itemIndex ON log (description ASC)"
-    ownidx = "CREATE INDEX IF NOT EXISTS ownIndex ON log (owner ASC)"
-    self.cursor.execute(tblstmt)
-    self.cursor.execute(itemidx)
-    self.cursor.execute(ownidx)
-    self.cursor.commit()
-    
-  def add_item(self, item_text, owner, date):
-    stmt = "INSERT INTO log (description, owner, date) VALUES (:description, :owner, :date)"
-    args = {"description":item_text, "owner":owner, "date":date}
-    self.cursor.execute(stmt, args)
-    self.cursor.commit()
+    def __init__(self):
+        self.dbname = os.environ['DATABASE_URL']
+        self.engine = create_engine(self.dbname)
+        self.cursor = self.session()
 
-  def delete_item(self, item_text, owner):
-    stmt = "DELETE FROM log WHERE description = :description AND owner = :owner"
-    args = {"description":item_text, "owner":owner}
-    self.cursor.execute(stmt, args)
-    self.cursor.commit()
+    def session(self):
+        return scoped_session(sessionmaker(bind=self.engine))
 
-  def get_items(self, owner):
-    stmt = "SELECT * FROM log WHERE owner = :owner ORDER BY date DESC LIMIT 15"
-    args = {"owner":owner}
-    results = self.cursor.execute(stmt, args)
-    return [x for x in results]
+    def setup(self):
+        tblstmt = "CREATE TABLE IF NOT EXISTS log (description text, owner integer, date timestamp)"
+        itemidx = "CREATE INDEX IF NOT EXISTS itemIndex ON log (description ASC)"
+        ownidx = "CREATE INDEX IF NOT EXISTS ownIndex ON log (owner ASC)"
+        try:
+            self.cursor.execute(tblstmt)
+            self.cursor.execute(itemidx)
+            self.cursor.execute(ownidx)
+            self.cursor.commit()
+        except:
+            self.cursor.rollback()
+            raise
+        finally:
+            self.cursor.close()
+
+    def add_item(self, item_text, owner, date):
+        stmt = "INSERT INTO log (description, owner, date) VALUES (:description, :owner, :date)"
+        args = {"description": item_text, "owner": owner, "date": date}
+        try:
+            self.cursor.execute(stmt, args)
+            self.cursor.commit()
+        except:
+            self.cursor.rollback()
+            raise
+        finally:
+            self.cursor.close()
+
+    def delete_item(self, item_text, owner):
+        stmt = "DELETE FROM log WHERE description = :description AND owner = :owner"
+        args = {"description": item_text, "owner": owner}
+        try:
+            self.cursor.execute(stmt, args)
+            self.cursor.commit()
+        except:
+            self.cursor.rollback()
+            raise
+        finally:
+            self.cursor.close()
+
+    def get_items(self, owner):
+        stmt = "SELECT * FROM log WHERE owner = :owner ORDER BY date DESC LIMIT 15"
+        args = {"owner": owner}
+        try:
+            results = self.cursor.execute(stmt, args)
+            return [x for x in results]
+        except:
+            self.cursor.rollback()
+            raise
+        finally:
+            self.cursor.close()
